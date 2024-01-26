88167bc811d56461681f9c750882159f34965f31
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-01-26 20:51:04 +0000

    Added first módule and made initial setup

---
+++ Get started with C#, Part 1/WriteYourFirstCode/WriteYourFirstCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteYourFirstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // link : https://learn.microsoft.com/en-us/training/modules/csharp-write-first/
+
+
+        }
+    }
+}
