a127c97e73c287ee440964bf9b1688b16713afed
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-01-26 23:50:40 +0000

    Finished Unit-4 of Module-02

---
+++ Get started with C#, Part 1/Module-02/Unit-3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Link: https://learn.microsoft.com/en-us/training/modules/csharp-literals-variables/3-declaring-variables
+
+            // Declare variables
+            // A literal is literally a hard - coded value.
+            // Hard - coded values, are values that are constant and unchanged throughout the execution of the program.
+            // However, most applications will require you to work with values that you don't know much about ahead of time.
+            // In other words, you'll need to work with data that comes from users, from files, or from across the network.
+
+            // When you need to work with data that isn't hard-coded, you'll declare a variable.
+
+            // What is a variable ?
+            // A variable is a container for storing a type of value.
+            // Variables are important because their values can change, or vary, throughout the execution of a program.
+            // Variables can be assigned, read, and changed.You use variables to store values that you intend to use in your code.
+
+            // A variable name is a human - friendly label that the compiler assigns to a memory address.
+            // When you want to store or change a value in that memory address,
+            // or whenever you want to retrieve the stored value, you just use the variable name you created.
+
+            //Declare a variable
+            //To create a new variable, you must first declare the data type of the variable, and then give it a name.
+            string firstName;
+            /*
+             * In this case, you're creating a new variable of type string called firstName. 
+             * From now on, this variable can only hold string values.
+             * You can choose any name as long as it adheres to a few C# syntax rules for naming variables.
+             */
+            // Variable name examples
+
+            char userOption;
+
+            int gameScore;
+
+            decimal particlesPerMillion;
+
+            bool processedCustomer;
+
+            // Recap:
+
+
+            // Here's what you've learned so far about variables:
+            // Variables are temporary values you store in the computer's memory.
+            // Before you can use a variable, you have to declare it.
+            // To declare a variable, you first select a data type for the kind of data you want to store,
+            // and then give the variable a name that follows the rules.
+
+
+        }
+    }
+}
---
+++ Get started with C#, Part 1/Module-02/Unit-4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // link: https://learn.microsoft.com/en-us/training/modules/csharp-literals-variables/4-exercise-variables
+
+
+            // Exercise - Setting and getting values from variables
+            // Because variables are temporary storage containers for data, they're meant to be written to and read from.
+            // You'll get a chance to do both in the following exercise.
+
+            // Exercise - Working with variables
+            // In this exercise, you'll declare a variable, assign it a value, retrieve its value, and more.
+            // Create your first variable
+            string firstName;
+            firstName = "Bob";
+          
+            // To declare a variable, you enter the data type you want to use followed by a name for the variable.
+            // To assign a value to a variable, you use the assignment operator, which is a single equals character =.
+
+            Console.WriteLine(firstName);
+
+            // Reassign the value of a variable
+            // You can reuse and reassign the variable as many times as you want.This example illustrates that idea.
+            firstName = "Liem";
+            Console.WriteLine(firstName);
+            firstName = "Isabella";
+            Console.WriteLine(firstName);
+            firstName = "Yasmin";
+            Console.WriteLine(firstName);
+
+            // To declare and iniatilize the variable in order to avoid the possibility of an unassigned local variable,
+            // it is recommended that you set the value as soon as possible after you declare it.
+            // Like so;
+
+            string myName = "Carlos";
+            Console.WriteLine(myName);
+
+            // Recap
+            // Here's what you've learned about working with variables so far:
+            // You must assign(set)
+            // a value to a variable before you can retrieve(get)
+            // a value from a variable.
+            // You can initialize a variable by assigning a value to the variable at the point of declaration.
+            // Assignment happens from right to left.
+            // You use a single equals character as the assignment operator.
+            // To retrieve the value from the variable, you merely use the variable's name.
+
+        }
+    }
+}
