8d016a9e6c2be0ef3aa262dfea78350589962785
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-01-27 11:34:30 +0000

    Completed Module-03

---
+++ Get started with C#, Part 1/Module-03/Unit-4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //link:  https://learn.microsoft.com/en-us/training/modules/csharp-basic-formatting/4-exercise-string-interpolation
+
+            // Exercise - Combine strings using string interpolation
+            /*
+             *  While string concatenation is simple and convenient, 
+             *  string interpolation is growing in popularity in situations 
+             *  where you need to combine many literal strings and variables into a single formatted message.
+             *  
+             *  What is string interpolation?
+             *  String interpolation combines multiple values into a single literal string
+             *  by using a "template" and one or more interpolation expressions. 
+             *  An interpolation expression is indicated by an opening and closing curly brace symbol { }. 
+             *  You can put any C# expression that returns a value inside the braces. 
+             *  The literal string becomes a template when it's prefixed by the $ character.
+             *  In other words, instead of writing the following lines of code:
+             */
+
+            /*
+            string firstName = "Bob";
+            string greeting = "Hello";
+            Console.WriteLine(greeting + " " + firstName + "!");
+            */
+
+            //  Write this instead
+
+            //string firstName = "Bob";
+            //string greeting = "Hello";
+            //string message = $"{greeting} {firstName}!";
+            //Console.WriteLine(message);
+
+            /*
+             * In this simple example, you save a few keystrokes. 
+             * You can imagine how much more concise string interpolation can be in more complex operations.
+             * Moreover, many find the string interpolation syntax cleaner and easier to read.
+             * 
+             * In the following exercise, you'll rewrite the previous messages using string interpolation.
+            */
+
+            // Use string interpolation to combine a literal string and a variable value
+            //To interpolate two strings together, you create a literal string and prefix the string with the $ symbol.
+            //The literal string should contain at least one set of curly braces { }
+            //and inside of those characters you use the name of a variable.
+
+            string firstName = "Bob";
+            string message = $"Hello {firstName}";
+            Console.WriteLine(message);
+
+            // Use string interpolation with multiple variables and literal strings
+            //You can perform several interpolation operations in the same line of code.
+            int version = 11;
+            string updateText = "Update to Windows";
+            //string updateMessage = $"{updateText} {version}!";
+           // Console.WriteLine(updateMessage);
+
+            // Avoid intermediate variables
+            // Just as you did in the previous exercise,
+            // you can eliminate the temporary variable to store the message.
+            Console.WriteLine($"{updateText} {version}!");
+
+            //Combine verbatim literals and string interpolation
+            //Suppose you need to use a verbatim literal in your template.
+            //You can use both the verbatim literal prefix symbol @ and the string interpolation $ symbol together.
+
+            string projectName = "First-Project";
+            Console.WriteLine($@"C:\Output\{projectName}\Data");
+            // In this example,
+            // the $ symbol allows you to reference the projectName variable inside the braces,
+            // while the @ symbol allows you to use the unescaped \ character.
+
+            /* Recap:
+             *   Here's what you've learned about string interpolation so far:
+             *   String interpolation provides an improvement over string concatenation by reducing the number of characters required in some situations.
+             *   You can combine string interpolation and verbatim literals by combining the symbols for each and using that as a prefix for the string template.
+             */
+
+        }
+    }
+}
---
+++ Get started with C#, Part 1/Module-03/Unit-5/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // Link: https://learn.microsoft.com/en-us/training/modules/csharp-basic-formatting/5-challenge
+
+            // Complete the challenge
+            // Code challenges will reinforce what you've learned and help you gain some confidence before continuing on.
+            // In this challenge,
+            // you'll print instructions to the end user to let them know where your application will output data files.
+            // You won't be actually creating any files --you're only interested in displaying formatted instructions to the console window.
+            // You'll use what you've learned about character escape sequences,
+            // verbatim strings, unicode, and string interpolation to provide instructions in both English and Russian.
+            //
+            // Challenge: Format and display instructions
+            // Begin solving the challenge with the following two lines of code.
+
+            string projectName = "ACME";
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434:";
+            /*
+             * The projectName variable will be used twice in the desired output.
+             * The russianMessage variable contains the message "View Russian output" in Russian. 
+             * You must use this variable in your code that prints the message.
+             * You may not change these two lines of code, but you can add code above and below each line. 
+             * You must use these two lines of code to form the desired output.
+             * You may only use either the Console.WriteLine() or the Console.Write() method twice.
+             * In other words, 
+             * to complete this challenge, 
+             * you can only create two instructions that actually print output to the console. 
+             * If you need to print additional new lines or add any formatting, 
+             * you must use what you've learned in this module to accomplish it.
+             * Use character escape sequences, verbatim strings, unicode, 
+             * and string interpolation to generate the output.
+             * 
+             * To complete this challenge, your code must produce the following output.
+             * View English output:
+             *        c:\Exercise\ACME\data.txt
+             * Посмотреть русский вывод:
+             *        c:\Exercise\ACME\ru-RU\data.txt
+             */
+
+            // Format encoding output to console first
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string englishOutput = "View English output:\n\t ";
+            string englishLocation = $@"C:\Exercise\{projectName}\data.text";
+            Console.Write(englishOutput);
+            Console.WriteLine(englishLocation);
+
+            string russianOutput = $"{russianMessage}\n\t";
+            string russianLocation = $@"C:\Exercise\{projectName}\ru-RU\data.text";
+            Console.WriteLine($"{russianOutput} {russianLocation}");
+
+
+        }
+    }
+}
