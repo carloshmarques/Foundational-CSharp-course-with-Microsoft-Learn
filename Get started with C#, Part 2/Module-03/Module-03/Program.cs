e5886a0897d2676a2a3006423867a5a1c5743785
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-01-31 23:25:35 +0000

    Started module 03

---
+++ Get started with C#, Part 2/Module-03/Module-03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  // Link: https://learn.microsoft.com/en-us/training/modules/csharp-if-elseif-else/
+            // Unit-
+        }
+    }
+}
