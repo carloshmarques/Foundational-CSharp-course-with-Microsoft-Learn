b7ac1599732a25181939c40faffc65aa79ae125a
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-03 19:45:04 +0000

    Started unit-3

--- Get started with C#, Part 2/Module-04/Module-04/Program.cs
+++ Get started with C#, Part 2/Module-04/Module-04/Program.cs
@@ -10,6 +10,32 @@ namespace Module_04
     {
         static void Main(string[] args)
         {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-arrays/
+            // Store and iterate through sequences of data using Arrays and the foreach statement in C#
+
+            // Declare a new array
+            // string[] fraudulentOrderIDs = new string[3];
+
+            // Initialize an array
+            string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+            // Assign values to elements of an array
+            //fraudulentOrderIDs[0] = "A123";
+            //fraudulentOrderIDs[1] = "B456";
+            //fraudulentOrderIDs[2] = "C789";
+
+            // Retrieve values from elements of an array
+            Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+            Console.WriteLine($"First: {fraudulentOrderIDs[1]}"); 
+            Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+            // Reassign the value of an array
+            fraudulentOrderIDs[0] = "F000";
+
+            Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+            // Use the Length property of an array
+            Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
         }
     }
 }
---
+++ Get started with C#, Part 2/Module-04/Unit-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // https://learn.microsoft.com/en-us/training/modules/csharp-arrays/3-exercise-foreach
+            // Exercise - Implement the foreach statement
+
+            // Create and initialize an array of int
+
+            int[] inventory = { 200, 450, 700, 175, 250 };
+
+            // Add a variable to sum the value of each element in the array
+            int sum = 0;
+
+            // Create a variable to hold the current bin number and display the running total
+            int bin = 0;
+
+            // Add a foreach statement to iterate through the array
+            foreach (int items in inventory)
+            {
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+
+            }
+            Console.WriteLine($"We have {sum} items in inventory.");
+
+        }
+    }
+}
