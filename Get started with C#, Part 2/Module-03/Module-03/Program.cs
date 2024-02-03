a423edd33bd8708e486af1671ffcd7388c193b13
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-03 15:54:19 +0000

    started exercise

--- Get started with C#, Part 2/Module-03/Module-03/Program.cs
+++ Get started with C#, Part 2/Module-03/Module-03/Program.cs
@@ -23,20 +23,32 @@ namespace Module_03
             int total = roll1 + roll2 + roll3;
 
             Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
+            // if statement to implement the doubles bonus
+            if ((roll1 ==  roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+            {
+                Console.WriteLine("You rolled doubles! +2 bonus total!" );
+                total += 2;
+            }
+            // if statement to implement the triples bonus
+            if((roll1 == roll2) && (roll2 == roll3))
+            {
+                Console.WriteLine("You rolled triples! +6 bonus total!");
+                total += 6;
+            }
             // Add an if statement to display different messages based on the value of the total variable
-            if (total > 14)
+
+            if (total >= 15) 
             { 
-                Console.WriteLine("You win!"); 
+                Console.WriteLine("Congrats, you win!"); 
             }
             if (total < 15)
             {
-                Console.WriteLine("You lose!");
+                Console.WriteLine("Sorry, you lose!");
             }
 
             // 
             // 
-            // Continue here:   https://learn.microsoft.com/en-us/training/modules/csharp-if-elseif-else/2-exercise-if
+            // Continue here: https://learn.microsoft.com/en-us/training/modules/csharp-if-elseif-else/2-exercise-if
 
 
 
