fb1f45b6e63ec6a2630fa90915597226e2c617a5
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-07 23:11:28 +0000

    Finished module-06

--- Get started with C#, Part 2/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Get started with C#, Part 2/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -2,24 +2,22 @@
 using System.Runtime.CompilerServices;
 
 // initialize variables - graded assignments 
-int currentAssignments = 5;
+int examAssignments = 5;
 
-// Sophia assignement score's array 
-int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
 
-// Andrew assignement score's array 
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
-// Emma assignement score's array 
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 
-// Logan assignement score's array
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
-
-// Create a student names array and outer foreach loop
 
 //Student names:
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 // Nested loop to calculate all student scores
 int[] studentScores = new int[10];
@@ -46,22 +44,48 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         studentScores = loganScores;
 
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+
+    else
+
+        continue;
+
     // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
 
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
+    // initialize/reset a counter for the number of assignment 
+    int gradedAssignments = 0;
+
     foreach (int score in studentScores)
     {
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if(gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+        else
+            // add the extra credit points to the sum -
+            // bonus points equal to 10% of an exam score
+            sumAssignmentScores += score/ 10;
 
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
-    // Calculate the and assign student's Grade 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
 
@@ -69,9 +93,12 @@ foreach (string name in studentNames)
         currentStudentLetterGrade = "A";
 
     else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "B+";
+        currentStudentLetterGrade = "A-";
 
     else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
         currentStudentLetterGrade = "B";
 
     else if (currentStudentGrade >= 80)
@@ -98,9 +125,11 @@ foreach (string name in studentNames)
     else
         currentStudentLetterGrade = "F";
 
+
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 
     // continue here: https://learn.microsoft.com/en-us/training/modules/guided-project-arrays-iteration-selection/5-exercise-implement-selection-statements
 }
+// required for running in VS Code (keeps the Output windows open to view results)
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
--- Get started with C#, Part 2/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Get started with C#, Part 2/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc338c5e899b89b9624f0a6a0b46b958db892270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b84970074f3f47e3e011f5b6b32ff9bd87a869c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
