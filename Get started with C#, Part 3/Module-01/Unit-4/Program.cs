ad414494ff389cf55c7952557f95bf225405fb01
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-10 15:50:26 +0000

    Started Unit-4

---
+++ Get started with C#, Part 3/Module-01/Unit-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/4-challenge-1
+            // 
+            // Complete a challenge activity using conditional operators
+
+
+        }
+    }
+}
