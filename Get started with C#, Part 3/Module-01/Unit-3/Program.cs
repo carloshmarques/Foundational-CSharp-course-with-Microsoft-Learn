f4a63db22a326cd93f80aaa914097f8640f5b1a1
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-10 15:27:27 +0000

    finished unit-3

--- Get started with C#, Part 3/Module-01/Unit-3/Program.cs
+++ Get started with C#, Part 3/Module-01/Unit-3/Program.cs
@@ -14,7 +14,23 @@ namespace Unit_3
 
             // Implement the conditional operator
 
+            // Add code that uses a conditional operator
+            int saleAmount = 1001;
+            //int discount = saleAmount > 1000 ? 100 : 50;
 
+            // Use the conditional operator inline
+
+            Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+            /*
+             * Recap
+             * You should remember the following facts about the conditional operator:
+             * You can use the conditional operator to reduce the size of your code, 
+             * but you should ensure that the resulting code is easily readable.
+             * You can use the conditional operator when you need to return a value that's based on a binary condition. 
+             * Your code will return the first option when the condition evaluates to true, 
+             * and it will return the second option when the condition evaluates to false.
+             */
         }
     }
 }
