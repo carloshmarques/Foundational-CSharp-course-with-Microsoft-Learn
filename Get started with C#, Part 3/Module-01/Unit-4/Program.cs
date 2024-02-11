109e22af133b0ffbda9a4c668f01a425d06bb6b9
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-11 17:09:36 +0000

    Finished Module-01

--- Get started with C#, Part 3/Module-01/Unit-4/Program.cs
+++ Get started with C#, Part 3/Module-01/Unit-4/Program.cs
@@ -13,6 +13,23 @@ namespace Unit_4
             // https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/4-challenge-1
             // 
             // Complete a challenge activity using conditional operators
+            /*
+             * Code challenge: write code to display the result of a coin flip
+             * 
+             * Here are your challenge requirements:
+             * Use the Random class to generate a value.
+             * Consider the range of numbers that is required.
+             * Based on the value generated, use the conditional operator to display either heads or tails.
+             * There should be a 50% chance that the result is either heads or tails.
+             * Your code should be easy to read, but with as few lines as possible.
+             * You should be able to accomplish the desired result in three lines of code.
+             * Whether you get stuck and need to peek at the solution or you finish successfully, continue on to view a solution to this challenge.
+             */
+            // Code here:
+            Random coin = new Random();
+            int coinFlip = coin.Next(0, 2);
+            Console.WriteLine((coinFlip == 0) ? "heads" : "tails" );
+            // Review the Solution  on: https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/5-solution-1
 
 
         }
---
+++ Get started with C#, Part 3/Module-01/Unit-6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/6-challenge-2
+            // Exercise - Complete a challenge activity using Boolean expressions
+
+            // Code here:
+            string permission = "Admin|Manager";
+            int level = 53;
+
+            if (permission.Contains("Admin"))
+            {
+                
+                if(level > 55)
+                {
+                    Console.WriteLine("Welcome, Super Admin user.");
+                }
+                else 
+                {
+                    Console.WriteLine("Welcome, Admin.");
+                }
+            }
+            else if (permission.Contains("Manager"))
+            {
+                if (level >= 20)
+                {
+                    Console.WriteLine("Contact an Admin for access.");
+                }
+                else
+                {
+                    Console.WriteLine("You do not have sufficient privileges.");
+                }
+
+            }
+            else 
+            {
+                Console.WriteLine("You do not have sufficient privileges.");
+            }
+        }
+    }
+}
