f08ec3939db29c787c50f3f71a807050264bfff0
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-12 22:22:51 +0000

    Started Unit-3 of module-o1

---
+++ Get started with C#, Part 5/Module-01/Module-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Link: https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/
+
+            // Write your first C# method
+            int[] a = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Contents of Array:");
+            PrintArray();
+
+            void PrintArray()
+            {
+                foreach (int x in a)
+                {
+                    Console.Write($"{x} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
