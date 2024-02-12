1dee0c9a89f7bb0631d59a275a98b61bdaa61b15
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-12 22:00:14 +0000

    started unit-3 of Module-02

--- Get started with C#, Part 3/Module-02/Module-02/Program.cs
+++ Get started with C#, Part 3/Module-02/Module-02/Program.cs
@@ -10,6 +10,23 @@ namespace Module_02
     {
         static void Main(string[] args)
         {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/
+            // Unit-2
+            // Link: https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/2-exercise-variable-scope
+
+            // Exercise - Code blocks and variable scope
+
+            // Create a variable inside of a code block
+            bool flag = true;
+            int value = 0;
+            if (flag)
+            {
+                
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+            value = 10;
+            Console.WriteLine($"Outside the code block: {value}");
+
         }
     }
 }
---
+++ Get started with C#, Part 3/Module-02/Unit-3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/3-exercise-remove-if-statements
+            // Exercise - Remove code blocks from if statements
+        }
+    }
+}
