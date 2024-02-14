000ce55a45f5a818422ee8fbc8d85374a3b6c3b6
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-14 00:48:52 +0000

    Added initial code!

--- Get started with C#, Part 3/Module-03/Module-03/Program.cs
+++ Get started with C#, Part 3/Module-03/Module-03/Program.cs
@@ -14,7 +14,40 @@ namespace Module_03
             // Branch the flow of code using the switch-case construct in C#
             // Unit-1
             // https://learn.microsoft.com/en-us/training/modules/csharp-switch-case/1-introduction
-            // Continue here:
+            // Unit-2
+            // https://learn.microsoft.com/en-us/training/modules/csharp-switch-case/2-exercise-switch-case
+
+            // Exercise - Implement a switch statement
+            // Create and test a switch statement
+            // Code here:
+            int employeeLevel = 200;
+            string employeeName = "John Smith";
+
+            string title = "";
+
+            switch (employeeLevel)
+            {
+                case 100:
+                    title = "Junior Associate";
+                    break;
+                case 200:
+                    title = "Senior Associate";
+                    break;
+                case 300:
+                    title = "Manager";
+                    break;
+                case 400:
+                    title = "Senior Manager";
+                    break;
+                default:
+                    title = "Associate";
+                    break;
+            }
+
+            Console.WriteLine($"{employeeName}, {title}");
+
+            // Continue: tomorrow
+
         }
     }
 }
