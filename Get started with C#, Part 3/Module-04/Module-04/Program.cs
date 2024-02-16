b40ebb210acdec3409870fe2225a9d7920695079
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-16 18:44:22 +0000

    Started Unit-3

--- Get started with C#, Part 3/Module-04/Module-04/Program.cs
+++ Get started with C#, Part 3/Module-04/Module-04/Program.cs
@@ -41,22 +41,49 @@ namespace Module_04
             */
 
             // Use the break keyword to break the iteration statement
+            /*
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
                 if (i == 7) break;
             }
+            */
 
             // Loop through each element of an array
-
+            /*
             string[] names = { "Alex", "Eddie", "David", "Michael" };
             for (int i = names.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(names[i]);
             }
+            */
 
             // Overcoming the limitation of the foreach statement using the for statement
-            // Continue here:
+            string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "David")
+                {
+                    names[i] = "Sammy";
+                }
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            /*
+             *  Recap
+             *  Here are a few of the takeaways from this unit:
+             *  The for iteration statement allows you to iterate through a block of code a specific number of times.
+             *  The for iteration statement allows you to control every aspect of the iteration's mechanics by 
+             *  altering the three conditions inside the parentheses: the initializer, condition, and iterator.
+             *  It's common to use the for statement when you need to control how you want to iterate through each item in an array.
+             *  If your code block has only one line of code, 
+             *  you can eliminate the curly braces and white space if you wish.
+             */
         }
     }
 }
---
+++ Get started with C#, Part 3/Module-04/Unit-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise - Complete a challenge activity using for and if statements
+            // linK : https://learn.microsoft.com/en-us/training/modules/csharp-for/3-challenge
+
+            // Code challenge - implement the FizzBuzz challenge rules
+
+        }
+    }
+}
