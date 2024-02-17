612bb7a2cc088c83a9c7681a1659b40a5f566a89
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-17 00:59:16 +0000

    Finished Module-04

--- Get started with C#, Part 3/Module-04/Unit-3/Program.cs
+++ Get started with C#, Part 3/Module-04/Unit-3/Program.cs
@@ -14,7 +14,26 @@ namespace Unit_3
             // linK : https://learn.microsoft.com/en-us/training/modules/csharp-for/3-challenge
 
             // Code challenge - implement the FizzBuzz challenge rules
-
+            for (int i = 0; i < 101; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine($"{i} - FizzBuzz");
+                }
+                else if ((i % 3 == 0))
+                {
+                    Console.WriteLine($"{i} - Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine($"{i} - Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}");
+                }
+                
+            }
         }
     }
 }
