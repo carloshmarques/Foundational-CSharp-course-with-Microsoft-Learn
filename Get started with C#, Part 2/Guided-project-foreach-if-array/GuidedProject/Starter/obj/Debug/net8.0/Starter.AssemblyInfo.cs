a1c805e3b54fc5b73bd84cfa69ad1a427a039ceb
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-23 21:24:45 +0000

    Finished Final Module

--- Get started with C#, Part 2/Guided-project-foreach-if-array/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Get started with C#, Part 2/Guided-project-foreach-if-array/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eabf78440d1b1cf2b183a3dd514dff72706338b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10305dae42d699ba652724dd7c48ff81aaa2fa99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
