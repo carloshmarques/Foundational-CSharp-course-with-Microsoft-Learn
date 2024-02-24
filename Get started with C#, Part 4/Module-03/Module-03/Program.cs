be55f39f325fcac3e8671f5947f200484a123332
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-24 14:55:18 +0000

    Started Module-03

---
+++ Get started with C#, Part 4/Module-03/Module-03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // https://learn.microsoft.com/en-us/training/modules/csharp-format-strings/
+            // Unit-1
+            //  https://learn.microsoft.com/en-us/training/modules/csharp-format-strings/1-introduction
+        }
+    }
+}
