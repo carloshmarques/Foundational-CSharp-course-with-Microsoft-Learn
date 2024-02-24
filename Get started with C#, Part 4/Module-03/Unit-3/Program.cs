10a8919b319d80c7b63d719811925a16a28ce465
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-24 15:29:53 +0000

    Finished Unit-3

--- Get started with C#, Part 4/Module-03/Unit-3/Program.cs
+++ Get started with C#, Part 4/Module-03/Unit-3/Program.cs
@@ -10,6 +10,30 @@ namespace Unit_3
     {
         static void Main(string[] args)
         {
+            //https://learn.microsoft.com/en-us/training/modules/csharp-format-strings/3-exercise-string-interpolation
+            // Exercise - Explore string interpolation
+
+            // Display the invoice number using string interpolation
+            int invoiceNumber = 1201;
+            decimal productShares = 25.4568m;
+            decimal subtotal = 2750.00m;
+            decimal taxPercentage = .15825m;
+            decimal total = 3185.19m;
+
+            Console.WriteLine($"Invoice Number: {invoiceNumber}");
+
+            // Display the product shares with one thousandth of a share (0.001) precision
+            Console.WriteLine($"    Shares: {productShares:N3} Product");
+
+            // Display the subtotal that you charge the customer formatted as currency
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"     Sub Total: {subtotal:C}");
+
+            // Display the tax charged on the sale formatted as a percentage
+            Console.WriteLine($"           Tax: {taxPercentage:P2}");
+
+            // Finalize the receipt with the total amount due formatted as currency
+            Console.WriteLine($"     Total Billed: {total:C}");
         }
     }
 }
