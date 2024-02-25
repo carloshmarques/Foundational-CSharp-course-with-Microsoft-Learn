0eb6322463545ed0abc39d42ceff9c461309bdae
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-25 19:18:20 +0000

    Finished Unit-3

--- Get started with C#, Part 4/Module-05/Unit-3/Program.cs
+++ Get started with C#, Part 4/Module-05/Unit-3/Program.cs
@@ -12,7 +12,46 @@ namespace Unit_3
         {
             // https://learn.microsoft.com/en-us/training/modules/csharp-convert-cast/3-exercise-tryparse
             // Exercise - Examine the TryParse() method
-            
+
+            // TryParse() a string into an int
+            /*
+            string value = "102";
+            int result = 0;
+            if (int.TryParse(value, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+            */
+            string value = "102";
+            int result = 0;
+            if (int.TryParse(value, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+            Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+            // Modify the string variable to a value that can't be parsed
+            string myValue = "bad";
+      
+            if (int.TryParse(myValue, out result))
+            {
+                Console.WriteLine($"Measurement: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to report the measurement.");
+            }
+
+            if (result > 0)
+                Console.WriteLine($"Measurement (w/ offset): {50 + result}");
         }
     }
 }
