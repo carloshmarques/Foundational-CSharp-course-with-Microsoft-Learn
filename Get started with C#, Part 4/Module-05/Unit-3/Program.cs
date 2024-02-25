b1c90b6aa65856a9e75163b54e630730a4b764ef
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-25 12:39:42 +0000

    Finished Unit-2 and started unit-3

---
+++ Get started with C#, Part 4/Module-05/Unit-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/csharp-convert-cast/3-exercise-tryparse
+            // Exercise - Examine the TryParse() method
+            
+        }
+    }
+}
