6d764935481bb07793a5e7e567c889ece60fd28a
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-25 19:46:55 +0000

    Finished Unit-6

--- Get started with C#, Part 4/Module-05/Unit-6/Program.cs
+++ Get started with C#, Part 4/Module-05/Unit-6/Program.cs
@@ -14,18 +14,20 @@ namespace Unit_6
             // Unit-6
 
             // Exercise - Complete a challenge to output math operations as specific number types
+
             int value1 = 12;
             decimal value2 = 6.2m;
             float value3 = 4.3f;
 
-            // Your code here to set result1
-            // Hint: You need to round the result to nearest integer (don't just truncate)
+            // The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+            // Convert.ToInt32() rounds up the way you would expect.
+            int result1 = Convert.ToInt32((decimal)value1 / value2);
             Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-            // Your code here to set result2
+            decimal result2 = value2 / (decimal)value3;
             Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-            // Your code here to set result3
+            float result3 = value3 / value1;
             Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
         }
     }
