c40c14099a5f464ddfe37989be359584b664afc8
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-26 22:27:24 +0000

    Restarted unit-3

--- Get started with C#, Part 5/Module-01/Module-01/Program.cs
+++ Get started with C#, Part 5/Module-01/Module-01/Program.cs
@@ -12,6 +12,11 @@ namespace Module_01
         {
             // Link: https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/
 
+
+            // Unit-2
+            // Understand the syntax of methods
+            // https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/2-understand-syntax-of-methods
+
             // Write your first C# method
             int[] a = { 1, 2, 3, 4, 5 };
 
@@ -26,6 +31,36 @@ namespace Module_01
                 }
                 Console.WriteLine();
             }
+
+            // Method execution
+            Console.WriteLine("Before calling a method");
+            SayHello();
+            Console.WriteLine("After calling a method");
+
+            void SayHello()
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            /*
+             *  Best practices
+             *  When choosing a method name, 
+             *  it's important to keep the name concise and make it clear what task the method performs.
+             *  Method names should be Pascal case and generally shouldn't start with digits. 
+             *  Names for parameters should describe what kind of information the parameter represents.
+             *  Consider the following method signatures:
+             */
+
+             void ShowData(string a, int b, int c)
+            {
+
+            }
+             void DisplayDate(string month, int day, int year)
+            {
+
+            }
+
+
         }
     }
 }
--- Get started with C#, Part 5/Module-01/Unit-3/Program.cs
+++ Get started with C#, Part 5/Module-01/Unit-3/Program.cs
@@ -12,6 +12,17 @@ namespace Unit_3
         {
             // https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/3-exercise-create-your-first-method
             // Exercise - Create your first method
+            // Create a method to display random numbers
+            void DisplayRandomNumbers()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < 5; i++) 
+                {
+                   Console.Write($"{random.Next(1, 100)} ");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
