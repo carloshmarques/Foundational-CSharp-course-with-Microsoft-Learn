19024e001eddd1ea4baed15121d4c3c787ddbce8
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-26 23:56:17 +0000

    Started Unit-6

---
+++ Get started with C#, Part 5/Module-01/Unit-6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace Unit_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise - Complete the challenge to create a reusable method
+
+        }
+    }
+}
