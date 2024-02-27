4779e47fe06a5483f56d1dcf08cf258dd7b19880
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 17:49:33 +0000

    Completed Unit-3

--- Get started with C#, Part 5/Module-03/Unit-3/Program.cs
+++ Get started with C#, Part 5/Module-03/Unit-3/Program.cs
@@ -13,7 +13,27 @@ namespace Unit_3
             // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/3-exercise-create-methods-return-numbers
             // Unit-3
             // Exercise - Return numbers from methods
+            // Create a method that returns an integer
+
+            double usd = 23.73;
+            int vnd = UsdToVnd(usd);
+
+            Console.WriteLine($"${usd} USD = ${vnd} VND");
+            Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
 
         }
+
+        static int UsdToVnd(double usd)
+        {
+            int rate = 23500;
+            return (int)(rate * usd);
+        }
+
+        static double VndToUsd(int vnd)
+        {
+            double rate = 23500;
+            return vnd / rate;
+        }
+
     }
 }
