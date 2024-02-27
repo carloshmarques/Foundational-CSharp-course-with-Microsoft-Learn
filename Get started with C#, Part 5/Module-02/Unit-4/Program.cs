a20288e5cb714228cb24697ae677d226553730ae
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 16:13:20 +0000

    Started Unit-5

--- Get started with C#, Part 5/Module-02/Unit-4/Program.cs
+++ Get started with C#, Part 5/Module-02/Unit-4/Program.cs
@@ -10,7 +10,82 @@ namespace Unit_4
     {
         static void Main(string[] args)
         {
-            // 
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope
+            // Unit-4
+            // Exercise - Use value and reference type parameters
+
+            // Parameters passed by value and passed by reference
+
+            // Test pass by value
+            int a = 3;
+            int b = 4;
+            int c = 0;
+            
+            Multiply(a, b, c);
+            Console.WriteLine($"global statement: {a} x {b} = {c}");
+            // Test pass by reference
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            PrintArray(array);
+            Clear(array);
+            PrintArray(array);
+
+            
+
+            // Test with strings
+
+            string status = "Healthy";
+
+            Console.WriteLine($"Start: {status}");
+            SetHealth(false);
+            Console.WriteLine($"End: {status}");
+
+            void SetHealth(bool isHealthy)
+            {
+                status = (isHealthy ? "Healthy" : "Unhealthy");
+                Console.WriteLine($"Middle: {status}");
+            }
+
+
+
+
         }
+
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int a in array)
+            {
+                Console.Write($"{a} ");
+            }
+            Console.WriteLine();
+        }
+
+       static void Clear(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+        }
+
+        static void Multiply(int a, int b, int c)
+        {
+            c = a * b;
+            Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+        }
+
+        /*
+         * Recap
+            Here's what you've learned about value type and reference type parameters so far:
+
+            Variables can be categorized as value types and reference types.
+            Value types directly contain values, and reference types store the address of the value.
+            Methods using value type arguments create their own copy of the values.
+            Methods that perform changes on an array parameter affect the original input array.
+            String is an immutable reference type.
+            Methods that perform changes on a string parameter don't affect the original string.
+         */
     }
 }
---
+++ Get started with C#, Part 5/Module-02/Unit-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/5-exercise-use-named-optional-parameters
+            // Exercise - Methods with optional parameters
+
+            // Create an RSVP application
+            string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+            string[] rsvps = new string[10];
+            int count = 0;
+
+            void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+            {
+                if (inviteOnly)
+                {
+                    // search guestList before adding rsvp
+                }
+
+                rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+                count++;
+            }
+
+            void ShowRSVPs()
+            {
+                Console.WriteLine("\nTotal RSVPs:");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine(rsvps[i]);
+                }
+            }
+        }
+    }
+}
