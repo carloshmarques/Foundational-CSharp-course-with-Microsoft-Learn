08651a63b59fc197f99fd9a9f35d95b1f22b3e0b
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 19:39:04 +0000

    Started Unit-7

---
+++ Get started with C#, Part 5/Module-03/Unit-7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/7-exercise-challenge-dice-game
+            // Exercise - Complete the challenge to add methods to make the game playable
+            // Dice mini-game challenge
+
+            // Code challenge: add methods to make the game playable
+
+        }
+    }
+}
