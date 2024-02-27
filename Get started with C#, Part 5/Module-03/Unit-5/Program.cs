7719fe83384f8465d061c87cae56332a2e2dcda4
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 19:19:32 +0000

    Finished Unit-5

--- Get started with C#, Part 5/Module-03/Unit-5/Program.cs
+++ Get started with C#, Part 5/Module-03/Unit-5/Program.cs
@@ -10,6 +10,33 @@ namespace Unit_5
     {
         static void Main(string[] args)
         {
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/5-exercise-create-methods-return-booleans
+            // Exercise - Return Booleans from methods
+
+            // Create a method that returns a Boolean
+            string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
+
+            Console.WriteLine("Is it a palindrome?");
+            foreach (string word in words)
+            {
+                Console.WriteLine($"{word}: {IsPalindrome(word)}");
+            }
+            bool IsPalindrome(string word)
+            {
+                int start = 0;
+                int end = word.Length - 1;
+
+                while (start < end)
+                {
+                    if (word[start] != word[end])
+                    {
+                        return false;
+                    }
+                    start++;
+                    end--;
+                }
+                return true;
+            }
         }
     }
 }
