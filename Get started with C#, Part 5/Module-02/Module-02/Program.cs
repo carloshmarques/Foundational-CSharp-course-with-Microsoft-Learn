592f8e25f147891eeb9b308107711a06db6060a2
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 00:31:26 +0000

    Started Module-02

---
+++ Get started with C#, Part 5/Module-02/Module-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/2-exercise-add-parameters-to-methods
+            // Unit-2
+            // Exercise - Use parameters in methods
+
+            // Add parameters to methods
+            CountTo(5);
+
+            void CountTo(int max)
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    Console.Write($"${i}, ");
+                }
+            }
+        }
+    }
+}
