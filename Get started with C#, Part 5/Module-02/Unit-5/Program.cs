4d0eff38e9c0579a595462b97ba9f30813597865
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 16:23:03 +0000

    Finished Unit-5

--- Get started with C#, Part 5/Module-02/Unit-5/Program.cs
+++ Get started with C#, Part 5/Module-02/Unit-5/Program.cs
@@ -18,11 +18,37 @@ namespace Unit_5
             string[] rsvps = new string[10];
             int count = 0;
 
-            void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+
+            RSVP("Rebecca");
+            RSVP("Nadia", 2, "Nuts");
+            RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+            RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+            RSVP("Noor", 4, inviteOnly: false);
+            RSVP("Jonte", 2, "Stone fruit", false);
+            ShowRSVPs();
+
+
+            // Declare optional parameters
+            //void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+            void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
             {
                 if (inviteOnly)
                 {
                     // search guestList before adding rsvp
+                    bool found = false;
+                    foreach (string guest in guestList)
+                    {
+                        if (guest.Equals(name))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        Console.WriteLine($"Sorry, {name} is not on the guest list");
+                        return;
+                    }
                 }
 
                 rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
@@ -37,6 +63,15 @@ namespace Unit_5
                     Console.WriteLine(rsvps[i]);
                 }
             }
+
+            /*
+             * Recap
+                Here's what you've learned about optional and named arguments so far:
+
+                Parameters are made optional by setting a default value in the method signature.
+                Named arguments are specified with the parameter name, followed by a colon and the argument value.
+                When combining named and positional arguments, you must use the correct order of parameters.
+             */
         }
     }
 }
