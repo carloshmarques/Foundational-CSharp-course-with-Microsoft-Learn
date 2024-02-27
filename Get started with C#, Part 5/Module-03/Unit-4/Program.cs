4d1c4c6c87b7e734159e8ae8eac6b65a394b5e6b
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 18:09:09 +0000

    Completed Unit-4

--- Get started with C#, Part 5/Module-03/Unit-4/Program.cs
+++ Get started with C#, Part 5/Module-03/Unit-4/Program.cs
@@ -11,6 +11,36 @@ namespace Unit_4
         static void Main(string[] args)
         {
             // Exercise - Return strings from methods
+            // Create a method that returns a string
+
+            // Test your code:
+            string input = "there are snakes at the zoo";
+
+            Console.WriteLine(input);
+            Console.WriteLine(ReverseSentence(input));           
         }
+        static string ReverseSentence(string input)
+        {
+            string result = "";
+            string[] words = input.Split(' ');
+            foreach (string word in words)
+            {
+                result += ReverseWord(word) + " ";
+            }
+            return result.Trim();
+        }
+
+        static string ReverseWord(string word)
+        {
+            string result = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                result += word[i];
+            }
+            return result;
+        }
+
+
+
     }
 }
