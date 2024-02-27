28e849d71cd02aa39ad2c8f868389d4407e0b4e2
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-02-27 15:55:51 +0000

    Finished Unit-3

--- Get started with C#, Part 5/Module-02/Module-02/Program.cs
+++ Get started with C#, Part 5/Module-02/Module-02/Program.cs
@@ -10,6 +10,12 @@ namespace Module_02
     {
         static void Main(string[] args)
         {
+            // Create C# methods with parameters
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/
+
+            // Unit-1
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/1-introduction
+
             // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/2-exercise-add-parameters-to-methods
             // Unit-2
             // Exercise - Use parameters in methods
@@ -24,7 +30,46 @@ namespace Module_02
                     Console.Write($"${i}, ");
                 }
             }
+
             // Continue here:
+            // Create a method with parameters
+            int[] schedule = { 800, 1200, 1600, 2000 };
+
+            DisplayAdjustedTimes(schedule, 6, -6);
+
+            void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+            {
+                int diff = 0;
+                if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+                {
+                    Console.WriteLine("Invalid GMT");
+                }
+                else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+                {
+                    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+                }
+                else
+                {
+                    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+                }
+
+                for (int i = 0; i < times.Length; i++)
+                {
+                    int newTime = ((times[i] + diff)) % 2400;
+                    Console.WriteLine($"{times[i]} -> {newTime}");
+                }
+
+            }
+            /*
+             * Recap
+                Here's what you've learned about parameters so far:
+
+                Information can be passed to methods in the form of parameters.
+                Parameters are declared in the method signature.
+                Multiple parameters are separated by commas.
+                Methods can accept variable or literal arguments.
+             */
+
         }
     }
 }
---
+++ Get started with C#, Part 5/Module-02/Unit-3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope
+            // Unit-3
+            // Exercise - Understand method scope
+
+            // Test variable scope
+            string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+            DisplayStudents(students);
+            DisplayStudents(new string[] { "Robert", "Vanya" });
+
+            double pi = 3.14159;
+            PrintCircleInfo(12);
+            PrintCircleInfo(24);
+
+            void PrintCircleInfo(int radius)
+            {
+                Console.WriteLine($"Circle with radius {radius}");
+                PrintCircleArea(radius);
+                PrintCircleCircumference(radius);
+            }
+
+            void PrintCircleArea(int radius)
+            {
+                double area = pi * (radius * radius);
+                Console.WriteLine($"Area = {area}");
+            }
+
+            void PrintCircleCircumference(int radius)
+            {
+                double circumference = 2 * pi * radius;
+                Console.WriteLine($"Circumference = {circumference}");
+            }
+
+
+        }
+    
+        static void DisplayStudents(string[] students)
+        {
+            foreach (string student in students)
+            {
+                Console.Write($"{student}, ");
+            }
+            Console.WriteLine();
+        }
+
+        /*
+         * Recap
+            Here's what you've learned about method scope so far:
+
+            Variables declared inside of a method are only accessible to that method.
+            Variables declared in top-level statements are accessible throughout the program.
+            Methods don't have access to variables defined within different methods.
+            Methods can call other methods.
+         */
+    }
+}
