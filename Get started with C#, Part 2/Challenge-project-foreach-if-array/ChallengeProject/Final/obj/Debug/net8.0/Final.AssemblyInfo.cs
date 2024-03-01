edece5772844941ccd3a354260dc48154382d5cb
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-01 18:48:36 +0000

    Started Module-02

--- Get started with C#, Part 2/Challenge-project-foreach-if-array/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Get started with C#, Part 2/Challenge-project-foreach-if-array/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc0875f37a7f727d26e610709c2b6956ad94f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e0146009b7e82b97bf1e3c6d881b015a47ebed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
